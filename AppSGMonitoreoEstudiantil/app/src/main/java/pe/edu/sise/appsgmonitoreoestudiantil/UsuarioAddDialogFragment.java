f0b0dda41e5c0f0aef4535618739b99a748c4ba5
Abel Palomino <abelpr87@gmail.com>
2017-03-05 05:02:50 -0500

    Enlanzando controles del Usuario xml con la clase java v1.0

--- AppSGMonitoreoEstudiantil/app/src/main/java/pe/edu/sise/appsgmonitoreoestudiantil/UsuarioAddDialogFragment.java
+++ AppSGMonitoreoEstudiantil/app/src/main/java/pe/edu/sise/appsgmonitoreoestudiantil/UsuarioAddDialogFragment.java
@@ -1,29 +1,82 @@
 package pe.edu.sise.appsgmonitoreoestudiantil;
 
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class UsuarioAddDialogFragment extends Fragment {
+public class UsuarioAddDialogFragment extends DialogFragment implements View.OnClickListener {
+    //constante - nombre del la clase para imprimir las excepciones
+    protected static final String TAG = "UsuarioAddDialog";
 
+    // Variables
+    Container container = new Container();
 
     public UsuarioAddDialogFragment() {
         // Required empty public constructor
     }
 
 
+    @NonNull
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_usuario_add_dialog, container, false);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        //Creacion de la vista
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_usuario_add_dialog, null);
+        container.edtxNombres = (EditText) view.findViewById(R.id.edtx_usu_add_nombres);
+        container.edtxApellidos = (EditText) view.findViewById(R.id.edtx_usu_add_apellidos);
+        container.edtxDniAlumno = (EditText) view.findViewById(R.id.edtx_usu_add_dni_alumno);
+        container.edtxEmail = (EditText) view.findViewById(R.id.edtx_usu_add_email);
+        container.edtxPassword = (EditText) view.findViewById(R.id.edtx_usu_add_password);
+        container.edtxPassConf = (EditText) view.findViewById(R.id.edtx_usu_add_pass_conf);
+        container.btnRegistrarse = (Button) view.findViewById(R.id.btn_usu_add_registarse);
+        view.setTag(container);
+
+        //Creacion del dialogo
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(view);
+
+        //Listeners
+        container.btnRegistrarse.setOnClickListener(this);
+
+        return builder.create();
+    }
+
+
+
+    private void registrarUsuario() {
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_usu_add_registarse:
+                registrarUsuario();
+                break;
+        }
+    }
+
+
+    //Clase privada para contener los controles
+    private class Container {
+        protected EditText edtxNombres;
+        protected EditText edtxApellidos;
+        protected EditText edtxDniAlumno;
+        protected EditText edtxEmail;
+        protected EditText edtxPassword;
+        protected EditText edtxPassConf;
+        protected Button btnRegistrarse;
     }
 
 }
