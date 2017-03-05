0b88dc7bdc880f304a82ab08b720192238a6c721
jhonatan17av <jhonatan17av@gmail.com>
2017-03-05 05:46:10 -0500

    Diseñando login 3

--- AppSGMonitoreoEstudiantil/app/src/main/java/pe/edu/sise/appsgmonitoreoestudiantil/LoginActivity.java
+++ AppSGMonitoreoEstudiantil/app/src/main/java/pe/edu/sise/appsgmonitoreoestudiantil/LoginActivity.java
@@ -4,11 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText log_etx_usu;
     private EditText log_etx_pass;
     private Button log_btn_entrar;
+    private TextView log_tvi_reg;
+    private TextView log_tvi_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +25,7 @@ public class LoginActivity extends AppCompatActivity {
         log_etx_usu = (EditText) findViewById(R.id.log_etx_usu);
         log_etx_pass = (EditText) findViewById(R.id.log_etx_pass);
         log_btn_entrar = (Button) findViewById(R.id.log_btn_entrar);
+        log_tvi_reg = (TextView) findViewById(R.id.log_tvi_reg);
+        log_tvi_pass = (TextView) findViewById(R.id.log_etx_pass);
     }
 }
