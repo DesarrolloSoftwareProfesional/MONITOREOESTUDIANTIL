382e164af2dd7bc362bf0bd20c689f21ad701390
Abel Palomino <abelpr87@gmail.com>
2017-03-05 03:28:29 -0500

    Diseño del registro del usuario v1.0

---
+++ AppSGMonitoreoEstudiantil/app/src/main/java/pe/edu/sise/appsgmonitoreoestudiantil/UsuarioAddDialogFragment.java
@@ -0,0 +1,29 @@
+package pe.edu.sise.appsgmonitoreoestudiantil;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class UsuarioAddDialogFragment extends Fragment {
+
+
+    public UsuarioAddDialogFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_usuario_add_dialog, container, false);
+    }
+
+}
