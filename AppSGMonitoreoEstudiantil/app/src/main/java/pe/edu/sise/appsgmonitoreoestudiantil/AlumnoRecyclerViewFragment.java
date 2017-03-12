15ea437dc720ccfe1f3f2873e79091ebdcfaab17
Abel Palomino <abelpr87@gmail.com>
2017-03-12 03:36:40 -0500

    add menu v1

---
+++ AppSGMonitoreoEstudiantil/app/src/main/java/pe/edu/sise/appsgmonitoreoestudiantil/AlumnoRecyclerViewFragment.java
@@ -0,0 +1,29 @@
+package pe.edu.sise.appsgmonitoreoestudiantil;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AlumnoRecyclerViewFragment extends Fragment {
+
+
+    public AlumnoRecyclerViewFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_alumno_recycler_view, container, false);
+    }
+
+}
