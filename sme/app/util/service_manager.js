a60a53afc89b8942339191f56ff7bfc63fe2eb73
jhonatan17av <jhonatan17av@gmail.com>
2017-05-01 18:48:31 -0500

    Servicio actualizado

--- sme/app/util/service_manager.js
+++ sme/app/util/service_manager.js
@@ -11,9 +11,9 @@ const ACCION_ACTUALIZAR = "Actualizar";
 
 const URL_BASE = "http://localhost/sme/api/";
 
-const URL_ALUMNO_LISTAR = URL_BASE + "alumno/listar";
-const URL_ALUMNO_BUSCAR = URL_BASE + "alumno/buscar/";
-const URL_ALUMNO_REGISTAR = URL_BASE + "alumno/registrar";
-const URL_ALUMNO_ACTUALIZAR = URL_BASE + "alumno/actualizar";
-const URL_ALUMNO_ELIMINAR = URL_BASE + "alumno/eliminar/";
-const URL_ALUMNO_LOGEO = URL_BASE + "alumno/logeo";
+const ALUMNO_URL_LISTAR = URL_BASE + "alumno/listar";
+const ALUMNO_URL_BUSCAR = URL_BASE + "alumno/buscar/";
+const ALUMNO_URL_REGISTAR = URL_BASE + "alumno/registrar";
+const ALUMNO_URL_ACTUALIZAR = URL_BASE + "alumno/actualizar";
+const ALUMNO_URL_ELIMINAR = URL_BASE + "alumno/eliminar/";
+const ALUMNO_URL_LOGEO = URL_BASE + "alumno/logeo";
