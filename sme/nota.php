81e44e21a85b9cfb48b2c119f9c5847e82a968de
Ronald Fabian Garcia <ronaldi_99@hotmail.com>
2017-06-18 22:47:28 -0500

    Detalle Nota

--- sme/nota.php
+++ sme/nota.php
@@ -15,69 +15,33 @@
 <body>
 <?php include_once 'template/main-menu.php'; ?>
 	<div class="container">
-	 <div id="divbotones">
-	 	<button type="button" class="btn btn-warning" id="btnRegistroNotas">Registrar Notas</button>
-	 </div>
-	 <hr>
-	 <div id="divTabla">
-	 	<table class="table table-striped table-responsive table-bordered table-condensed table-hover">
-	 		<thead>
-	 		<tr class="warning">
-	 			<th>Apellido Paterno</th>
-	 			<th>Apellido Materno</th>
-	 			<th>Nombres</th>
-	 			<th>Nro Documento</th>
-	 			<th>Fecha de Nacimiento</th>
-	 			<th>Dirección</th>
-	 			<th>Grupo Académico</th>
-	 			<th colspan="2">Acciones</th>
-	 		</tr>
-	 	</thead>
-	 	<tbody id="tblAlumnos">
-	 		 
-	 	</tbody>
-	 	</table>
-	 </div>
-
+		<div id="divbotones">
+			<button type="button" class="btn btn-warning" id="btnRegistroNotas">Registrar Notas</button>
+		</div>
+		<hr>
+		<div id="divTabla">
+		 	<table class="table table-striped table-responsive table-bordered table-condensed table-hover">
+		 		<thead>
+			 		<tr class="warning">
+			 			<th>Apellido Paterno</th>
+			 			<th>Apellido Materno</th>
+			 			<th>Nombres</th>
+			 			<th>Nro Documento</th>
+			 			<th>Fecha de Nacimiento</th>
+			 			<th>Dirección</th>
+			 			<th>Grupo Académico</th>
+			 			<th colspan="2">Acciones</th>
+			 		</tr>
+			 	</thead>
+			 	<tbody id="tblAlumnos">
+			 		 
+			 	</tbody>
+		 	</table>
+		</div>
 	</div>
+
 <?php include_once 'template/main-footer.php'; ?>
-
-<!-- Modal de Registro -->
-<div class="modal fade" id="modalRegistroNotas" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-          <h4 class="modal-title" id="myModalLabel"><b>Registro de Notas</b></h4>
-        </div>
-        <form class=""  method="post">
-        <input type="hidden" id="id">
-        <div class="modal-body" id="datosAqui">
-          <div class="modal-body">
-
-            <div class="form-group">
-              <select class="form-control" id="slnCurso">
-              	<option value='0' disabled="selected"> Seleccione Curso </option>"
-              </select>
-            </div>
-
-              <div class="form-group">
-                <select class="form-control" id="slnPeriodo">
-                <option value='0' disabled="selected"> Seleccione Periodo </option>"
-                </select>
-              </div>
-          </div>
-          <div class="modal-footer">
-            <input type="button" class="btn btn-warning btn-block " id="guardarNota" value="Adicionar Nota">
-          </div>
-        </div>
-        </form>
-
-      </div>
-    </div>
-  </div>
-<!-- /Modal de Registro -->
-
+<?php include_once 'notaDetalle.php'; ?>
 
 	<script src="resources/js/vendor/modernizr-2.8.3.min.js"></script>
   	<script src="resources/js/vendor/jquery-1.11.2.min.js"></script>
---
+++ sme/notaDetalle.php
@@ -0,0 +1,59 @@
+<!-- Modal de Registro -->
+<div class="modal fade" id="modalRegistroNotas" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+		<div class="modal-content">
+	        <div class="modal-header">
+	          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+	          <h4 class="modal-title" id="myModalLabel"><b>Registro de Notas</b></h4>
+	        </div>
+	        <form class=""  method="post">
+		        <div class="modal-body" id="datosAqui">
+					<div class="modal-body">
+						<div class="form-group">
+						  <select class="form-control" id="slnCurso">
+						  	<option value='0' disabled selected> Seleccione Curso </option>"
+						  </select>
+						</div>
+
+						<div class="form-group">
+							<select class="form-control" id="slnPeriodo">
+								<option value='0' disabled selected> Seleccione Periodo </option>"
+							</select>
+						</div>
+						<div class="form-group">
+							<select class="form-control" id="slnAlumno">
+								<option value='0' disabled selected> Seleccione Alumno </option>"
+							</select>
+						</div>
+						<hr>
+						<div class="form-inline">
+							<label >Lista de Notas</label>
+							<input type="button" class="btn btn-warning pull-right" style="margin:-7px 5px 5px 5px" id="btnBuscarNotas" value="Buscar Notas">
+						</div>
+						
+						<div id="divTablaNotas">
+							<table class="table table-striped table-responsive table-bordered table-condensed table-hover">
+						 		<thead>
+							 		<tr class="warning">
+							 			<th>Tipo de Examen</th>
+							 			<th>Fecha de Registro</th>
+							 			<th>Nota</th>
+							 			<th colspan="2">Acciones</th>
+							 		</tr>
+							 	</thead>
+							 	<tbody id="tblNotas">
+							 		 
+							 	</tbody>
+						 	</table>
+						</div>
+					</div>
+					<div class="modal-footer">
+						<input type="button" class="btn btn-warning btn-block " id="btnguardarNota" value="Adicionar Nota">
+					</div>
+		        </div>
+	        </form>
+
+      	</div>
+    </div>
+ </div>
+<!-- /Modal de Registro -->
